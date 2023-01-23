37167c8d7364fc1ac16c720d922cac2bba88454e
Paulus Summer <paulus.summer@webundsoehne.com>
2023-01-23 16:14:48 +0100

    einheitenrechner

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const km = ref(0)
+const celsius = ref(0)
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="bg-stone-800 text-white flex flex-col justify-center items-center min-h-screen">
+    <div class="flex flex-col w-fit">
+      <div class="flex flex-wrap  justify-between items-center mb-4">
+        <input class="bg-stone-900 p-2 rounded-lg w-16" type="number" v-model="km">
+        <label for="km" class="px-1">km</label>
+        <span class="mr-auto">»</span>
+        <div class="px-1">Miles: {{ (km / 1.609).toFixed(2) }}</div>
+      </div>
+      <div class="flex flex-wrap justify-between items-center ">
+        <input class="bg-stone-900 p-2 rounded-lg w-16" type="number" v-model="celsius">
+        <label for="km" class="px-1">°C</label>
+        <span class="mr-auto">»</span>
+        <div class="px-1">Fahrenheit: {{ ((celsius * 9 / 5) + 32).toFixed(2) }}</div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
